ed08bd24e4d38cde08388e94c6fca7be7ea1ff54
potato0520 <potato20040520@gmail.com>
2021-12-13 22:01:48 +0900

    ..

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,16 @@
+import 'dart:io';
+import 'dart:ui';
 import 'package:flutter/material.dart';
-import 'package:easyissueboard/screens/team.dart';
+import 'package:easyissueboard/screens/mainscreen.dart';
 import 'package:window_size/window_size.dart';
-//import 'package:bitsdojo_window/bitsdojo_window.dart';
+import 'package:flutter_acrylic/flutter_acrylic.dart';
 
-void main() {
-  runApp(const MyApp());
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  setWindowMinSize(const Size(1280, 800));
+  await Window.initialize();
+  runApp(MyApp());
   setWindowMaxSize(const Size(1280, 800));
+  setWindowMinSize(const Size(1280, 800));
 }
 
 class MyApp extends StatelessWidget {
