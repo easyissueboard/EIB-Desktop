829091a5e03d34808da8c848a681a6d81cfd0b5e
potato0520 <potato20040520@gmail.com>
2021-12-14 16:47:50 +0900

    쓰레기 코드 제거 + 플로그린 제거

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,9 @@
-import 'dart:io';
-import 'dart:ui';
 import 'package:flutter/material.dart';
 import 'package:easyissueboard/screens/mainscreen.dart';
 import 'package:window_size/window_size.dart';
-import 'package:flutter_acrylic/flutter_acrylic.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Window.initialize();
   runApp(MyApp());
   setWindowMaxSize(const Size(1280, 800));
   setWindowMinSize(const Size(1280, 800));
@@ -20,7 +16,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Team(),
+      home: MainScreen(),
     );
   }
 }
--- windows/flutter/generated_plugins.cmake
+++ windows/flutter/generated_plugins.cmake
@@ -5,7 +5,6 @@
 list(APPEND FLUTTER_PLUGIN_LIST
   bitsdojo_window_windows
   desktop_window
-  flutter_acrylic
   window_size
 )
 
